758373978a88ecff6295221e2806abcb05992b5d
Alex Qian <qianalex8@gmail.com>
2022-02-24 13:20:12 -0700

    passed off server

--- src/main/java/handler/FileHandler.java
+++ src/main/java/handler/FileHandler.java
@@ -12,24 +12,47 @@ public class FileHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        if (exchange.getRequestMethod().toLowerCase().equals("get")) {
-            String urlPath = exchange.getRequestURI().toString();
-            if (urlPath == null || urlPath.equals("/")) {
-                urlPath = "/index.html";
-            }
-            String filePath = "web" + urlPath;
-            File file = new File(filePath);
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-            OutputStream respBody = exchange.getResponseBody();
-            if (!file.exists()) {
-                exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
-                File error = new File("web/HTML/404.html");
-                Files.copy(error.toPath(), respBody);
-            }
-            else {
+        boolean success = false;
+        try {
+            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+                String urlPath = exchange.getRequestURI().toString();
+                if (urlPath == null || urlPath.equals("/") || urlPath.equals("/index.html")) {
+                    urlPath = "/index.html";
+                }
+                else if (urlPath.equals("/css/main.css")) {
+                    urlPath = "/css/main.css";
+                }
+                else if (urlPath.equals("/favicon.ico")) {
+                    urlPath = "/favicon.ico";
+                }
+                else {
+                    urlPath = "/HTML/404.html";
+                }
+                String filePath = "web" + urlPath;
+                File file = new File(filePath);
+                OutputStream respBody = exchange.getResponseBody();
+
+                if (filePath.equals("web/HTML/404.html")) {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+                }
+                else {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                }
+
                 Files.copy(file.toPath(), respBody);
+
+                exchange.close();
+                success = true;
             }
-            exchange.close();
+            if (!success) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+                exchange.getResponseBody().close();
+            }
+        }
+        catch (IOException e) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            exchange.getResponseBody().close();
+            e.printStackTrace();
         }
     }
 
